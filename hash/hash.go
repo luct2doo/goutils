87a529f42410c155564bf9e9e108ed37b26d7ec1
luct2doo <10753132+luct2doo@users.noreply.github.com>
2026-09-16 23:52:02 +0800

    add hash/hash

---
+++ hash/hash.go
@@ -0,0 +1,31 @@
+package hash
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+
+	"github.com/luct2doo/goutils/logger"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func BcryptHash(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost+4)
+	logger.LogIf(err)
+	return string(hash), nil
+}
+
+// BcryptCheck 对比明文密码和数据库的哈希值
+func BcryptCheck(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func BcryptIsHashed(str string) bool {
+	return len(str) == 60
+}
+
+func MD5Hash(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
